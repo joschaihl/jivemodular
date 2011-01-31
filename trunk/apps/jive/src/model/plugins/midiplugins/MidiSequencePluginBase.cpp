68557a6be448e9b98f20469bd19da3017746dcd2
haszari <haszari@c1bc9126-2f0f-11df-afb1-bd2833558206>
2011-01-31 10:17:31 +0000

    support multiple midi clips in sequencers, live switchable via a midi bindable parameter split off midi bindings editor code into separate file

--- trunk/apps/jive/src/model/plugins/midiplugins/MidiSequencePluginBase.cpp
+++ trunk/apps/jive/src/model/plugins/midiplugins/MidiSequencePluginBase.cpp
@@ -32,22 +32,77 @@
 
 #define NOTE_VELOCITY      0.8f
 #define NOTE_PREFRAMES     0.001
+#define DEFAULT_MAXIMUM_CLIPS 16
 
+double D_getNoteOnIndexed (const MidiMessageSequence* midiSequence, int chanNum = -1, const int index = 0)
+{
+   int note = 0; float beat = 0.0;
+   float length = 0;
+    int numNoteOn = 0;
+    for (int i = 0; i < midiSequence->getNumEvents (); i++)
+    {
+        MidiMessageSequence::MidiEventHolder* eventOn = midiSequence->getEventPointer (i);
+        MidiMessageSequence::MidiEventHolder* eventOff = midiSequence->getEventPointer (i)->noteOffObject;
+
+        MidiMessage* msgOn = & eventOn->message;
+
+         if (chanNum != -1 && (msgOn->getChannel() != chanNum))
+            continue;
+
+        if (eventOn->message.isNoteOn () && eventOff)
+        {
+            MidiMessage* msgOff = & eventOff->message;
+            if (index == numNoteOn)
+            {
+                note = msgOn->getNoteNumber ();
+                beat = msgOn->getTimeStamp ();
+                length = ((msgOff->getTimeStamp () + NOTE_PREFRAMES) - msgOn->getTimeStamp ());
+                break;
+            }
+            numNoteOn++;
+        }
+    }
+
+   return length;
+}
+
+int D_getNumNoteOn (const MidiMessageSequence* midiSequence, int chanNum = -1)
+{
+    int numNoteOn = 0;
+
+    for (int i = 0; i < midiSequence->getNumEvents (); i++)
+        if (midiSequence->getEventPointer(i)->message.isNoteOn() && 
+            (chanNum == -1 || midiSequence->getEventPointer(i)->message.getChannel() == chanNum)) 
+         numNoteOn++;
+
+    return numNoteOn;
+}
 
 //==============================================================================
 MidiSequencePluginBase::MidiSequencePluginBase ()
   : transport (0),
     midiSequence (0),
+    allClipsByChannelSequence(0),
+    currentClip(0),
     doAllNotesOff (false),
     allNotesOff (MidiMessage::allNotesOff (1)),
     uptoBeatReloopHack(0.0)
 {
-    midiSequence = new MidiMessageSequence ();
+   midiSequence = new MidiMessageSequence();
+   allClipsByChannelSequence = new MidiMessageSequence();
+   
+   // our string array needs to be 16 big
+   for (int i=0; i<DEFAULT_MAXIMUM_CLIPS; i++)
+      clipFiles.add(String());
+   
+   // note that "current midi clip" bindable parameter is registered in leaf class
 }
 
 MidiSequencePluginBase::~MidiSequencePluginBase ()
 {
-    deleteAndZero (midiSequence);
+   deleteAndZero (midiSequence);
+   deleteAndZero (allClipsByChannelSequence);
+    
    removeAllParameters (true);
 }
 
@@ -109,6 +164,8 @@ void MidiSequencePluginBase::processBlock (AudioSampleBuffer& buffer,
 
       int midiChannel = getMidiChannel();
       
+      ScopedReadLock seqlock(midiPlaybackSequenceLock);
+      
       // normal case - the buffer occurs somewhere within the seq loop (i.e. start is before end!)
 		if (frameEndBeatCount > beatCount)
 		{
@@ -130,6 +187,7 @@ void MidiSequencePluginBase::processBlock (AudioSampleBuffer& buffer,
 
     if (doAllNotesOff || transport->willSendAllNotesOff ())
     {
+      noteOffs.clear();
         midiBuffer->addEvent (allNotesOff, blockSize - 1);
         doAllNotesOff = false;
     }
@@ -514,9 +572,9 @@ static void dumpMidiMessageSequence (MidiMessageSequence* midiSequence)
     {
         MidiMessage* midiMessage = &midiSequence->getEventPointer (i)->message;
 
-        DBG (String (midiMessage->getNoteNumber()) + " "
+        std::cerr << (String (midiMessage->getNoteNumber()) + " "
              + (midiMessage->isNoteOn() ? "ON " : "OFF ")
-             + String (midiMessage->getTimeStamp()));
+             + String (midiMessage->getTimeStamp())).toUTF8() << std::endl;
 
         // DBG ("Playing event @ " + String (frameCounter) + " : " + String (timeStamp));
     }
@@ -539,19 +597,13 @@ void MidiSequencePluginBase::getChunk (MemoryBlock& mb)
 {
     MidiFile midiFile;
 
-#if 0
-    MidiMessageSequence timeTrack;
-    timeTrack.addEvent (MidiMessage::timeSignatureMetaEvent (4, divDenominator));
-
-    // add timing informations sequence
-    midiFile.addTrack (timeTrack);
-#endif
+   pushCurrentEditedClipToAllClipsSequence(currentClip);
 
     // add real sequence scaled down !
     MidiMessageSequence scaledSequence;
-    for (int i = 0; i < midiSequence->getNumEvents (); i++)
+    for (int i = 0; i < allClipsByChannelSequence->getNumEvents (); i++)
     {
-        MidiMessage midiMessage = midiSequence->getEventPointer (i)->message;
+        MidiMessage midiMessage = allClipsByChannelSequence->getEventPointer (i)->message;
         midiMessage.setTimeStamp (midiMessage.getTimeStamp() * 100000.0f);
         scaledSequence.addEvent (midiMessage);
     }
@@ -565,53 +617,34 @@ void MidiSequencePluginBase::getChunk (MemoryBlock& mb)
 
 void MidiSequencePluginBase::setChunk (const MemoryBlock& mb)
 {
-    MemoryInputStream is (mb.getData(), mb.getSize(), false);
+   MemoryInputStream is (mb.getData(), mb.getSize(), false);
 
-    MidiFile midiFile;
-    midiFile.readFrom (is);
+   MidiFile midiFile;
+   midiFile.readFrom (is);
 
-#if 0
-    // use the first track as signature time track !
-    const MidiMessageSequence* timeSequence = midiFile.getTrack (0);
-    if (timeSequence)
-    {
-        for (int i = 0; i < timeSequence->getNumEvents (); i++)
-        {
-            MidiMessage* msg = & timeSequence->getEventPointer (i)->message;
-            if (msg->isTimeSignatureMetaEvent ())
-            {
-                int numerator, denominator;
-                msg->getTimeSignatureInfo (numerator, denominator);
-                setTimeSignature (bpmTempo, numBars, denominator);
-            }
-        }
-    }
+   allClipsByChannelSequence->clear ();
 
-    // add the note track !
-    const MidiMessageSequence* notesSequence = midiFile.getTrack (1);
-#endif
+   const MidiMessageSequence* notesSequence = midiFile.getTrack (0);
+   if (notesSequence)
+   {
+      // add real sequence scaled down !
+      for (int i = 0; i < notesSequence->getNumEvents (); i++)
+      {
+         MidiMessage midiMessage = notesSequence->getEventPointer (i)->message;
+         if (midiMessage.isNoteOnOrOff() || midiMessage.isController())
+         {
+            midiMessage.setTimeStamp (midiMessage.getTimeStamp() / 100000.0f);
+            allClipsByChannelSequence->addEvent (midiMessage);
+         }
+      }
+      if (transport->isPlaying ())
+         doAllNotesOff = true;
+   }
+   
+   allClipsByChannelSequence->updateMatchedPairs ();
+   getCurrentEditClipFromAllClipsSequence(currentClip);
 
-    const MidiMessageSequence* notesSequence = midiFile.getTrack (0);
-    if (notesSequence)
-    {
-        // add real sequence scaled down !
-        midiSequence->clear ();
-        for (int i = 0; i < notesSequence->getNumEvents (); i++)
-        {
-            MidiMessage midiMessage = notesSequence->getEventPointer (i)->message;
-            if (midiMessage.isNoteOnOrOff() || midiMessage.isController())
-            {
-                midiMessage.setTimeStamp (midiMessage.getTimeStamp() / 100000.0f);
-                midiSequence->addEvent (midiMessage);
-            }
-        }
-        midiSequence->updateMatchedPairs ();
-
-        if (transport->isPlaying ())
-            doAllNotesOff = true;
-    }
-
-    dumpMidiMessageSequence (midiSequence);
+   dumpMidiMessageSequence (midiSequence);
 }
 
 //==============================================================================
@@ -626,6 +659,17 @@ void MidiSequencePluginBase::savePropertiesToXml (XmlElement* xml)
     xml->setAttribute (PROP_SEQBAR,                  getIntValue (PROP_SEQBAR, 4));
     xml->setAttribute (PROP_SEQENABLED,              getBoolValue (PROP_SEQENABLED, true));
     xml->setAttribute (PROP_SEQMIDICHANNEL,          getIntValue (PROP_SEQMIDICHANNEL, 1));
+    xml->setAttribute (PROP_SEQCURRENTCLIP,          getCurrentClipIndex());
+    
+   XmlElement* clipsElement = new XmlElement(PROP_SEQCLIPFILES);
+   for (int i=0; i<jmin(clipFiles.size(), DEFAULT_MAXIMUM_CLIPS); i++)
+   {
+      XmlElement* clipEl = new XmlElement(PROP_SEQCLIPITEM);
+      clipEl->setAttribute(PROP_SEQCLIPINDEX, i);
+      clipEl->setAttribute(PROP_SEQCLIPFILE, clipFiles[i]);
+      clipsElement->addChildElement(clipEl);
+   }
+   xml->addChildElement(clipsElement);
 }
 
 void MidiSequencePluginBase::loadPropertiesFromXml (XmlElement* xml)
@@ -639,6 +683,18 @@ void MidiSequencePluginBase::loadPropertiesFromXml (XmlElement* xml)
     setValue (PROP_SEQBAR,                           xml->getIntAttribute (PROP_SEQBAR, 4));
     setValue (PROP_SEQENABLED,                       xml->getBoolAttribute (PROP_SEQENABLED, true));
     setValue (PROP_SEQMIDICHANNEL,                   xml->getIntAttribute (PROP_SEQMIDICHANNEL, 1));
+    currentClip = xml->getIntAttribute (PROP_SEQCURRENTCLIP, 0);
+
+   XmlElement* clipsElement = xml->getChildByName(PROP_SEQCLIPFILES);
+   if (clipsElement)
+   {
+      forEachXmlChildElementWithTagName (*clipsElement, e, PROP_SEQCLIPITEM)
+      {
+         int clipindex = e->getIntAttribute (PROP_SEQCLIPINDEX);
+         String clipfile = e->getStringAttribute (PROP_SEQCLIPFILE);
+         clipFiles.set(clipindex, clipfile);
+      }
+   }
 }
 
 //==============================================================================
@@ -647,13 +703,16 @@ AudioProcessorEditor* MidiSequencePluginBase::createEditor ()
     return new SequenceComponent (this);
 }
 
-void MidiSequencePluginBase::setSequence(const MidiMessageSequence* mseq)
+void MidiSequencePluginBase::setSequence(const MidiMessageSequence* mseq, int clipNumber)
 {
-	if (mseq)
+   if (clipNumber == -1)
+      clipNumber = getCurrentClipIndex();
+      
+	if (mseq && clipNumber >= 0 && clipNumber < 16)
 	{
-		deleteAndZero(midiSequence);
-		midiSequence = new MidiMessageSequence(*mseq);
-      midiSequence->updateMatchedPairs();
+      allClipsByChannelSequence->deleteMidiChannelMessages(clipNumber + 1);
+      allClipsByChannelSequence->addSequence(*mseq, 0, 0, DBL_MAX);
+      allClipsByChannelSequence->updateMatchedPairs();
 	}
 }
 
@@ -667,3 +726,199 @@ double MidiSequencePluginBase::getLoopBeatPosition()
 	return transport->getPositionInBeats() - (getLoopRepeatIndex() * getLengthInBeats()); 
 }
 
+//==============================================================================
+
+int MidiSequencePluginBase::getMaxUsedClipIndex()
+{
+   int maxClip = 0;
+   for (int i=16; i>0 && maxClip == 0; i--)
+   {
+      // if the clip's midi channel has some events
+      int clipChannel = i+1;
+      MidiMessageSequence tmp;
+      allClipsByChannelSequence->extractMidiChannelMessages(clipChannel, tmp, false);
+      if (tmp.getNumEvents() > 0)
+         maxClip = i; // found the last one, will cause loop break
+
+      // also if there is a clip file in a slot then it is a clip (even if it has no notes)
+      if (clipFiles[i].isNotEmpty())
+         maxClip = i;
+   }      
+   return maxClip;
+}
+
+String MidiSequencePluginBase::getClipFile(int clipIndex) const
+{
+   String currentFile = "<unnamed>";
+   
+   if (clipIndex >= 0 && clipIndex < clipFiles.size() && clipFiles[clipIndex].containsNonWhitespaceChars())
+      currentFile = clipFiles[clipIndex];
+      
+   return currentFile;
+}
+
+int MidiSequencePluginBase::getCurrentClipIndex()
+{
+   return currentClip;
+}
+
+void MidiSequencePluginBase::setCurrentClipIndex(int index, bool forceImportEvenIfSameAsCurrent)
+{
+   int oldClipIndex = currentClip;
+   if (index >= 0 && index < 16) // has to be a valid midi channel
+      currentClip = index;
+   int newClipIndex = currentClip;
+
+   if (allClipsByChannelSequence && (forceImportEvenIfSameAsCurrent || (newClipIndex != oldClipIndex)))
+   {
+      // copy any edits to current into the clip sequence
+      pushCurrentEditedClipToAllClipsSequence(oldClipIndex);
+      
+      // copy requested clip to play/edit/view sequence
+      getCurrentEditClipFromAllClipsSequence(newClipIndex);
+   }
+}
+
+void MidiSequencePluginBase::pushCurrentEditedClipToAllClipsSequence(int oldClipIndex)
+{
+   MidiMessageSequence currentPlayingClip;
+   {
+      ScopedReadLock seqlock(midiPlaybackSequenceLock);
+      currentPlayingClip = MidiMessageSequence(*midiSequence);
+   }
+   setClipMidiSequence(currentPlayingClip, oldClipIndex);
+}
+
+void MidiSequencePluginBase::getCurrentEditClipFromAllClipsSequence(int newClipIndex)
+{
+   MidiMessageSequence newSeq;
+   allClipsByChannelSequence->extractMidiChannelMessages(newClipIndex + 1, newSeq, false);
+   newSeq.updateMatchedPairs();
+
+   if (transport->isPlaying ())
+       doAllNotesOff = true;
+
+   {
+      ScopedWriteLock seqlock(midiPlaybackSequenceLock);
+      
+      deleteAndZero(midiSequence);
+      midiSequence = new MidiMessageSequence(newSeq);
+      midiSequence->updateMatchedPairs();
+   }
+}
+
+void MidiSequencePluginBase::moveClipFromIndexToIndex(int cur, int newIndex)
+{
+   // copy it to new location (if in range; otherwise this method implements remove!)
+   MidiMessageSequence tmp;
+   if (newIndex >= 0 && newIndex < 16)
+   {
+      int newChan = newIndex + 1;
+      // copy clip to move
+      allClipsByChannelSequence->extractMidiChannelMessages(cur+1, tmp, false);
+      // clear out destination clip slot
+      allClipsByChannelSequence->deleteMidiChannelMessages(newIndex+1);
+      // fix channel
+      for (int i=0; i<tmp.getNumEvents(); i++)
+         tmp.getEventPointer(i)->message.setChannel(newChan);
+      // copy it back in
+      allClipsByChannelSequence->addSequence(tmp, 0, 0, DBL_MAX);
+   }
+
+   // remove it from old location
+   if (cur >= 0 && cur < 16)
+      allClipsByChannelSequence->deleteMidiChannelMessages(cur+1);
+
+   allClipsByChannelSequence->updateMatchedPairs();
+}
+
+void MidiSequencePluginBase::setClipMidiSequence(const MidiMessageSequence& seqClip, int clipIndex)
+{
+   int clipChan = clipIndex + 1;
+   for (int i=0; i<seqClip.getNumEvents(); i++)
+   {
+      MidiMessageSequence::MidiEventHolder* event = seqClip.getEventPointer(i);
+      event->message.setChannel(clipChan);
+   }
+}
+
+void MidiSequencePluginBase::importMidiFileToClip(const File& file, int clipIndex)
+{
+   FileInputStream midiFileIn(file);
+   MidiFile droppedMidiFile;
+   droppedMidiFile.readFrom(midiFileIn);
+   droppedMidiFile.convertTimestampTicksToSeconds();
+   if (droppedMidiFile.getNumTracks() > 0)
+   {
+      MidiMessageSequence imported(*droppedMidiFile.getTrack(0));
+      imported.updateMatchedPairs(); // "readFrom"d clips do not get updated with cached noteoff stuff, this line is critical!
+      
+      for (int i=0; i<imported.getNumEvents(); i++)
+      {
+         MidiMessageSequence::MidiEventHolder* event = imported.getEventPointer(i);
+         event->message.setTimeStamp(event->message.getTimeStamp() * 2.0); // traditional workaround in seq gui import! (this 
+      }
+      setClipMidiSequence(imported, clipIndex);
+   }
+}
+
+void MidiSequencePluginBase::importClipFiles(const StringArray& files)
+{
+   int numNewClips = files.size();
+   
+   // ensure any edits to current clip get saved
+   pushCurrentEditedClipToAllClipsSequence(currentClip);
+    
+   // push existing clips down the list to make room for the new clips
+   // ensure we push the files AND the midi clips in allClipsByChannelSequence
+   for (int i=getMaxUsedClipIndex(); i>=0; i--)
+      moveClipFromIndexToIndex(i, i+numNewClips);
+   
+   // load the new files into the empty slots at the top of the list
+   for (int i=0; i<numNewClips; i++)
+   {
+      importMidiFileToClip(File(files[i]), numNewClips-i-1);
+      clipFiles.insert(0, files[i]);
+   }
+
+   getCurrentEditClipFromAllClipsSequence(0);
+   setCurrentClipIndex(0);
+
+   // ensure we don't have more than 16 files in the list
+   for (int i=clipFiles.size(); i>=DEFAULT_MAXIMUM_CLIPS; i--)
+      clipFiles.remove(i);
+}
+
+
+//==============================================================================
+
+void MidiSequencePluginBase::setParameterReal (int paramNumber, float value)
+{
+   if (paramNumber == MIDISEQ_PARAMID_CURRENTCLIP)
+   {
+      int newClipIndex = value * DEFAULT_MAXIMUM_CLIPS;
+      if (newClipIndex > getMaxUsedClipIndex())
+         newClipIndex = 0;
+      setCurrentClipIndex(newClipIndex);
+   }
+}
+
+float MidiSequencePluginBase::getParameterReal (int paramNumber)
+{
+   float value = 0.0f;
+   if (paramNumber == MIDISEQ_PARAMID_CURRENTCLIP)
+      value = static_cast<double>(currentClip) / DEFAULT_MAXIMUM_CLIPS;
+   return value;
+}
+
+const String MidiSequencePluginBase::getParameterTextReal (int paramNumber, float value)
+{
+   String paramTxt("");
+   if (paramNumber == MIDISEQ_PARAMID_CURRENTCLIP)
+   {
+      int newClipIndex = round(value * DEFAULT_MAXIMUM_CLIPS);
+      paramTxt = String(String(newClipIndex) + String(" ") + File(getClipFile(newClipIndex)).getFileNameWithoutExtension());
+   }
+
+   return paramTxt;
+}
